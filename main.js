bdce59f2bb1386c6422fc00757829322708adb03
veena-g123 <57742830+veena-g123@users.noreply.github.com>
2024-09-24 01:01:57 +0530

    Update main.js
    removed html+= in first line of iterateoverarray

--- main.js
+++ main.js
@@ -38,7 +38,6 @@ const iterateOverArray = inputArray => {
     let html = ''
 
     inputArray.forEach(item => {
-        html += 
         html +=`<div class="col-md-3 col-sm-6 col-xs-12 mb-4">`
         html +=    `<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${item}" data-instgrm-version="14" style="background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">`
         html +=        `<div style="padding:16px;">`
